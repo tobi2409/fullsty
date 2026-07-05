ce32b85858c831dc085b58b61226f29ee7425a04
Tobias Hollstein <tobias@git.com>
2026-07-05 17:40:19 +0200

    demo auth-test improved

--- projects/auth-test/src/client/index.js
+++ projects/auth-test/src/client/index.js
@@ -3,38 +3,50 @@
 import TemplateEngine from 'template-engine/template-engine.js'
 
 const data = TemplateEngine.reactive({
-    login: {
+    loginSection: {
         email: 'user@dummy.com',
         password: 'dummy',
+        statusMessage: '',
+        statusColor: 'black',
         okClick: async () => {
             try {
-                const token = await Server.loginUser(data.login.email, data.login.password)
-                alert(`Login successful. Token: ${token}`)
+                const token = await Server.loginUser(data.loginSection.email, data.loginSection.password)
+                data.loginSection.statusMessage = `Login successful. Token: ${token}`
+                data.loginSection.statusColor = 'green'
             } catch (error) {
-                alert(error)
+                data.loginSection.statusMessage = `Login failed. Error: ${error}`
+                data.loginSection.statusColor = 'red'
             }
         }
     },
-    register: {
+    registerSection: {
         email: 'user@dummy.com',
         password: 'dummy',
+        statusMessage: '',
+        statusColor: 'black',
         okClick: async () => {
             try {
-                const fetchedUser = await Server.registerUser(data.register.email, data.register.password)
-                alert(`New user registered with UUID: ${fetchedUser.uuid}`)
+                const fetchedUser = await Server.registerUser(data.registerSection.email, data.registerSection.password)
+                data.registerSection.statusMessage = `New user registered with UUID: ${fetchedUser.uuid}`
+                data.registerSection.statusColor = 'green'
             } catch (error) {
-                alert(error)
+                data.registerSection.statusMessage = `Registration failed. Error: ${error}`
+                data.registerSection.statusColor = 'red'
             }
         }
     },
-    currentUser: {
+    currentUserSection: {
         token: 'abc-0123-xyz',
+        statusMessage: '',
+        statusColor: 'black',
         okClick: async () => {
             try {
-                const uuid = await Server.getCurrentUser(data.currentUser.token)
-                alert(`Current user UUID: ${uuid}`)
+                const uuid = await Server.getCurrentUser(data.currentUserSection.token)
+                data.currentUserSection.statusMessage = `Current user UUID: ${uuid}`
+                data.currentUserSection.statusColor = 'green'
             } catch (error) {
-                alert(error)
+                data.currentUserSection.statusMessage = `Failed to get current user. Error: ${error}`
+                data.currentUserSection.statusColor = 'red'
             }
         }
     }
